8db18a540b2e0fd6f784298ddb5137cc6661615a
Theo <fanxiaobin422@gmail.com>
2024-07-28 15:17:44 +0800

    feat(style): optimize footer components

--- .vitepress/theme/components/HomeFooter.vue
+++ .vitepress/theme/components/HomeFooter.vue
@@ -1,132 +1,91 @@
 <template>
-    <footer class="bg-alt f12 lh-1" style="text-align: center; margin: 0 auto">
-        <div class="flex" style="display: inline-block">
-            <span class="ml1">
-                <a target="_blank" href="https://beian.miit.gov.cn/" name="ICP备案" title="ICP备案">
-                    <i class="fas fa-earth-americas" style="margin-right: 4px"></i>鄂ICP备2024060426号
-                </a>
-            </span>
-            <span class="ml1">
-                <a target="_blank" href="https://beian.mps.gov.cn/" name="公安备案" title="公安备案">
-                    <i class="fas fa-shield" style="margin-right: 4px"></i>粤公网安备44200102445439号
-                </a>
-            </span>
-        </div>
-        <div class="flex ma4" style="display: inline-block">
-            <span class="ml1">
-                Copyright <i class="far fa-copyright"></i> 2019 -
-                {{ new Date().getFullYear() }}
-                <a target="_blank" name="GitHub" title="GitHub" href="https://github.com/Theo-Messi">Theo-Messi</a>. All
-                Rights
-                Reserved
-            </span>
-        </div>
-    </footer>
+  <footer class="ba">
+    <!-- 底部信息栏 -->
+    <div class="ff" style="display: inline-block">
+      <span>
+        <a target="_blank" href="https://beian.miit.gov.cn/" name="ICP备案" title="ICP备案">
+          <i class="fas fa-earth-americas"></i>鄂ICP备2024060426号
+        </a>
+      </span>
+      <span>
+        <a target="_blank" href="https://beian.mps.gov.cn/" name="公安备案" title="公安备案">
+          <i class="fas fa-shield"></i>粤公网安备44200102445439号
+        </a>
+      </span>
+    </div>
+    <div class="flex" style="display: inline-block">
+      <span>
+        Copyright<i class="far fa-copyright"></i>2019 - {{ new Date().getFullYear() }}
+        <a target="_blank" name="GitHub" title="GitHub" href="https://github.com/Theo-Messi">Theo-Messi</a>. All Rights
+        Reserved
+      </span>
+    </div>
+  </footer>
 </template>
-
-<script lang="ts">
-
-
-</script>
 
 <style lang="scss" scoped>
 /* 页脚 */
 footer {
-    width: 100%;
+  width: 100%;
 
-    a:hover {
-        -webkit-text-decoration: underline dotted;
-        text-decoration: underline dotted;
-    }
+  a:hover {
+    -webkit-text-decoration: underline dotted;
+    text-decoration: underline dotted;
+  }
 
-    .has-sidebar~& {
-        display: none;
-    }
+  .has-sidebar~& {
+    display: none;
+  }
+}
+
+span {
+  margin-left: 1rem;
+}
+
+i {
+  margin-right: 0.25rem;
+  margin-left: 0.25rem;
 }
 
 .external-link {
-    position: relative;
-    display: inline-block;
+  position: relative;
+  display: inline-block;
 
-    &::after {
-        content: '\f061';
-        font-family: 'Font Awesome 6 Free';
-        font-weight: 900;
-        font-size: 0.5rem;
-        position: absolute;
-        color: var(--vp-c-text-3);
-        transform: rotate(-45deg);
-    }
+  &::after {
+    content: '\f061';
+    font-family: 'Font Awesome 6 Free';
+    font-weight: 900;
+    font-size: 0.5rem;
+    position: absolute;
+    color: var(--vp-c-text-3);
+    transform: rotate(-45deg);
+  }
 }
 
-.bg-alt {
-    background: var(--vp-c-bg-alt);
+.ba {
+  background: var(--vp-c-bg-alt);
+  font-size: 0.75rem;
+  text-align: center;
+  margin: 0 auto;
 }
 
-.lh-1 {
-    line-height: 1.2rem;
+.ff {
+  display: flex;
+  justify-content: center;
+  margin: 1.25rem;
+
+  &-grow-1 {
+    flex-grow: 1;
+  }
 }
 
 .flex {
-    display: flex;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1.25rem;
 
-    &-grow-1 {
-        flex-grow: 1;
-    }
-}
-
-.ma4 {
-    margin: 1.5rem;
-}
-
-.ma2 {
-    margin: 1rem;
-}
-
-.ml0 {
-    margin-left: 0.5rem;
-}
-
-.ml1 {
-    margin-left: 1rem;
-}
-
-.mb2 {
-    margin-bottom: 0.5rem;
-}
-
-.f12 {
-    font-size: 12px;
-}
-
-.f14 {
-    font-size: 13px;
-}
-
-.fw6 {
-    font-weight: 600;
-}
-
-.fb6 {
-    flex-basis: 20rem;
-}
-
-@media screen and (min-width: 960px) {
-    .flex-l {
-        display: flex;
-    }
-}
-
-@media screen and (max-width: 768px) {
-    .ma4 {
-        margin-left: auto;
-        /* 左边距自动 */
-        margin-right: auto;
-        /* 右边距自动 */
-        width: 100%;
-        /* 宽度100% */
-        max-width: 95%;
-        /* 最大宽度为屏幕宽度的95% */
-    }
+  &-grow-1 {
+    flex-grow: 1;
+  }
 }
 </style>
