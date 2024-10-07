5303a6e6caba44d222aae0cd0016c8a4d932923c
Theo <fanxiaobin422@gmail.com>
2024-10-07 20:42:51 +0800

    chore: Promotions

--- .vitepress/data/index.ts
+++ .vitepress/data/index.ts
@@ -1,2 +1,1 @@
-export * from './AsideData'
 export * from './FooterData'
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,32 +2,18 @@ import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import '@theojs/lumen/theme'
 
-import {
-  DocBox,
-  DocLinks,
-  DocBoxCube,
-  DocVideoLink,
-  Announcement,
-  DocAsideLogo,
-  HomeUnderline,
-  HomeFooter
-} from '@theojs/lumen'
-import { Aside_Data, Footer_Data } from '../data'
+import { Announcement, HomeUnderline, HomeFooter } from '@theojs/lumen'
+import { Footer_Data } from '../data'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'home-hero-info-before': () => h(Announcement),
-      'aside-ads-before': () => h(DocAsideLogo, { Aside_Data }),
       'layout-bottom': () => h(HomeFooter, { Footer_Data })
     })
   },
   enhanceApp: ({ app }) => {
-    app.component('Box', DocBox)
-    app.component('Links', DocLinks)
-    app.component('BoxCube', DocBoxCube)
     app.component('Home', HomeUnderline)
-    app.component('VideoLink', DocVideoLink)
   }
 }
